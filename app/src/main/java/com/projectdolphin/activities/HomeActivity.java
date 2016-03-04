96931dff213f8cae0b619728d5a143bb8408c543
alexv-anderson <opiemike613@yahoo.com>
2016-03-04 11:06:51 -0600

    Add LongClickListener

--- app/src/main/java/com/projectdolphin/activities/HomeActivity.java
+++ app/src/main/java/com/projectdolphin/activities/HomeActivity.java
@@ -1,8 +1,10 @@
 package com.projectdolphin.activities;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -51,6 +53,19 @@ public class HomeActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String s = ((TextView) view.findViewById(R.id.title)).getText().toString() + " at pos=" + position;
                 Toast.makeText(getBaseContext(), s, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Snackbar.make(view, "LongClick", Snackbar.LENGTH_SHORT).show();
+                int visibility = view.findViewById(R.id.right).getVisibility();
+                if(visibility != View.VISIBLE)
+                    view.findViewById(R.id.right).setVisibility(View.VISIBLE);
+                else
+                    view.findViewById(R.id.right).setVisibility(View.GONE);
+                return true;
             }
         });
 
