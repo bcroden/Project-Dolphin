5cadb17a676171fab0134294475f93fcc5a9c6a3
alexv-anderson <opiemike613@yahoo.com>
2016-03-10 11:08:41 -0600

    Elevate card

--- app/src/main/java/com/projectdolphin/layout/ClassRecycleAdapter.java
+++ app/src/main/java/com/projectdolphin/layout/ClassRecycleAdapter.java
@@ -1,5 +1,6 @@
 package com.projectdolphin.layout;
 
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
